bb3899effb457f9b159df9fba9329b8328450f48
LiquidM8 <95014292+LiquidM8@users.noreply.github.com>
2025-07-23 12:28:04 -0400

    Added NVIM function switch from lazy and custom

--- zsh/zshrc.d/20-alias.zsh
+++ zsh/zshrc.d/20-alias.zsh
@@ -3,7 +3,7 @@
 #secrets
 source $HOME/DotMatrix/.env/SECRETS
 
-
+# Colors and List
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias ll='ls -lah'
@@ -14,6 +14,19 @@ alias c='clear'
 #tmuxinator
 alias startw='tmuxinator start work'
 alias stopw='tmuxinator stop work'\
+
+#Linux
+alias serial='sudo picocom -b 9600 /dev/ttyUSB0'
+
+#NVIM env
+nvs() {
+  case "$1" in
+    lazy)    ln -sfn "$HOME/LiquiVim/nvim-lazy"   "$HOME/.config/nvim" ;;
+    custom)  ln -sfn "$HOME/LiquiVim/nvim-custom" "$HOME/.config/nvim" ;;
+    *)
+      echo "Usage: nvs lazy|custom" ;;
+  esac
+}
 
 #work
 alias NULLR='ssh $NULLR'
